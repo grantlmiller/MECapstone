b861003805bf6758eea952988f2386558c3b5beb
grantlmiller <gl1miller@yahoo.com>
2020-03-07 13:34:20 -0500

    added csv lookup table

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def add_popup():
     popup.wm_title("Add Element")
     label = tkinter.Label(popup, text="Add Element")
     label.pack(side="top", fill="x", pady=10)
-    B1 = tkinter.Button(popup, text="Okay", command = popup.destroy)
+    B1 = tkinter.Button(popup, text="Add", command = popup.destroy)
+    B2 = tkinter.Button(popup, text="Cancel", command = popup.destroy)
     B1.pack()
+    B2.pack()
     popup.mainloop()
 
 
