c41059a6be111c2564392457b3d3ed884d25154b
grantlmiller <gl1miller@yahoo.com>
2020-03-07 13:05:02 -0500

    Rearch of main window and added add_button popup

---
+++ main.py
@@ -0,0 +1,41 @@
+import tkinter
+import tkinter.messagebox
+import Element
+
+#global variables
+elementList = []
+#button functions 
+def add_clicked():
+    print("add clicked")
+    add_popup()
+def delete_clicked():
+    print("Delete clicked")
+
+#define pop up window 
+def add_popup():
+    popup = tkinter.Tk()
+    popup.wm_title("Add Element")
+    label = tkinter.Label(popup, text="Add Element")
+    label.pack(side="top", fill="x", pady=10)
+    B1 = tkinter.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+
+#establish main window
+mainWindow = tkinter.Tk()
+mainWindow.title("main tool")
+elementList = []
+
+#main listbox for elements 
+elementListBox = tkinter.Listbox(mainWindow)
+elementListBox.pack()
+#elementListBox.insert(END,"Hell")
+
+#buttons on main window
+addButton = tkinter.Button(mainWindow, text="Add", command=add_clicked)
+deleteButton = tkinter.Button(mainWindow, text="Delete", command=delete_clicked)
+addButton.pack()
+deleteButton.pack()
+#run window mainloop
+mainWindow.mainloop()
